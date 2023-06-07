4d67375110e0501e68554572fd5b4516705a2970
EhsanAsh <ehsan.ashrafipour@gmail.com>
2023-06-06 20:14:42 -0400

    finish option2 with asking

--- guessing_game.py
+++ guessing_game.py
@@ -1,7 +1,8 @@
+# Option 2: with asking to continue the game.
 from random import randint
 computer_guess = randint(1, 11)
-player_guess = None
-while player_guess != computer_guess:
+# player_guess = None
+while True:  # player_guess != computer_guess://it always will be true so we don't need to write it and the aboveline
     player_guess = int(input('Guess a number between 1 and 10: '))
     if player_guess < computer_guess:
         print('Too low, try again!')
@@ -9,4 +10,10 @@ while player_guess != computer_guess:
         print('Too high, try again!')
     else:
         print('You guessed it! You won!')
-print('Game over!')
+        play_again = input('Do you want to play again? (y/n) ').lower()
+        if play_again == 'y':
+            computer_guess = randint(1, 11)
+            player_guess = None
+        else:
+            print('Thank you for playing!')
+            break
